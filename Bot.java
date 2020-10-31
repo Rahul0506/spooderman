8a3be9e07b0b5aba5a57fe6c1e20c164142fb366
Rahul0506 <rahul.gidijala@gmail.com>
2020-10-31 15:51:00 +0800

    More production shenanigans

--- Bot.java
+++ Bot.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class Bot {
     public static void main(String[] args) {
-        Configuration config = new Configuration(false);
+        Configuration config = new Configuration(true);
         try
         {
             Socket skt = new Socket(config.exchange_name(), config.port());
