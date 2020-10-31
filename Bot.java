194466d2daf7921eb175547fbfb0e8c466edc5d5
Rahul0506 <rahul.gidijala@gmail.com>
2020-10-31 12:37:17 +0800

    Exchange logic for BOND

--- Bot.java
+++ Bot.java
@@ -5,10 +5,7 @@ import java.net.Socket;
 import java.util.Arrays;
 
 public class Bot {
-    public static void main(String[] args)
-    {
-        /* The boolean passed to the Configuration constructor dictates whether or not the
-           bot is connecting to the prod or test exchange. Be careful with this switch! */
+    public static void main(String[] args) {
         Configuration config = new Configuration(true);
         try
         {
@@ -16,17 +13,11 @@ public class Bot {
             BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
             PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
 
-            /*
-              A common mistake people make is to to_exchange.println() > 1
-              time for every from_exchange.readLine() response.
-              Since many write messages generate marketdata, this will cause an
-              exponential explosion in pending messages. Please, don't do that!
-            */
             to_exchange.println(("HELLO " + config.team_name).toUpperCase());
             String reply = from_exchange.readLine().trim();
             System.err.printf("The exchange replied: %s\n", reply);
 
-            Exchange exchange = new Exchange();
+            Exchange exchange = new Exchange(to_exchange);
             boolean keepGoing = true;
             while (keepGoing) {
                 String[] message = from_exchange.readLine().trim().split(" ");
@@ -38,29 +29,24 @@ public class Bot {
             e.printStackTrace(System.out);
         }
     }
-}
 
-class Configuration {
-    String exchange_name;
-    int    exchange_port;
-    /* 0 = prod-like
-       1 = slow
-       2 = empty
-    */
-    final Integer test_exchange_kind = 1;
-    /* replace REPLACEME with your team name! */
-    final String  team_name          = "SPOODERMAN";
+    static class Configuration {
+        String exchange_name;
+        int    exchange_port;
+        final Integer test_exchange_kind = 1;
+        final String  team_name          = "SPOODERMAN";
 
-    Configuration(Boolean test_mode) {
-        if(!test_mode) {
-            exchange_port = 20000;
-            exchange_name = "production";
-        } else {
-            exchange_port = 20000 + test_exchange_kind;
-            exchange_name = "test-exch-" + this.team_name;
+        Configuration(Boolean test_mode) {
+            if(!test_mode) {
+                exchange_port = 20000;
+                exchange_name = "production";
+            } else {
+                exchange_port = 20000 + test_exchange_kind;
+                exchange_name = "test-exch-" + this.team_name;
+            }
         }
-    }
 
-    String  exchange_name() { return exchange_name; }
-    Integer port()          { return exchange_port; }
+        String  exchange_name() { return exchange_name; }
+        Integer port()          { return exchange_port; }
+    }
 }
--- Exchange.java
+++ Exchange.java
@@ -1,3 +1,4 @@
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -5,30 +6,24 @@ import java.util.Map;
 public class Exchange {
 
     private static int idCounter = 0;
-    private Map<String, Integer> priceMap;
+    private final Map<String, Integer> priceMap;
+    private final PrintWriter to_exchange;
 
-    public Exchange(){
+    public Exchange(PrintWriter to_exchange){
+        this.to_exchange = to_exchange;
         priceMap = new HashMap<>(7);
+        priceMap.put("BOND", 1000);
+        addBuy("BOND", 999, 1);
     }
 
-    public String addBuy(String symb, int price, int size) {
+    public void addBuy(String symb, int price, int size) {
         idCounter += 1;
-        return String.format("ADD %d %s BUY %d %d", idCounter, symb, price, size);
+        to_exchange.print(String.format("ADD %d %s BUY %d %d\n", idCounter, symb, price, size));
     }
 
-    public String addSell(String symb, int price, int size) {
+    public void addSell(String symb, int price, int size) {
         idCounter += 1;
-        return String.format("ADD %d %s SELL %d %d", idCounter, symb, price, size);
-    }
-
-    public String buyBOND(int price, int size) {
-        idCounter += 1;
-        return String.format("ADD %d BOND BUY %d %d", idCounter, price, size);
-    }
-
-    public String sellBOND(int price, int size) {
-        idCounter += 1;
-        return String.format("ADD %d BOND SELL %d %d", idCounter, price, size);
+        to_exchange.print(String.format("ADD %d %s SELL %d %d\n", idCounter, symb, price, size));
     }
 
     public boolean parse(String[] message) {
@@ -36,12 +31,21 @@ public class Exchange {
             // ignore
         } else if (message[0].equals("TRADE")) {
             String symbol = message[1];
+            if (symbol.equals("BOND")) {
+                return true;
+            }
             int price = Integer.parseInt(message[2]);
             // int size = Integer.parseInt(message[3]);
             priceMap.put(symbol, price);
-            System.out.println(String.format("Updated %s to %d", symbol, price));
+            System.out.printf("Updated %s to %d%n", symbol, price);
         } else if (message[0].equals("FILL")) {
             System.out.println(Arrays.deepToString(message));
+            String symbol = message[2];
+            if (message[3].equals("BUY")) {
+                addSell(symbol, priceMap.get(symbol) - 1, 1);
+            } else {
+                addBuy(symbol, priceMap.get(symbol) + 1, 1);
+            }
         } else if (message[0].equals("REJECT")) {
             idCounter -= 1;
         } else if (message[0].equals("CLOSE")) {
